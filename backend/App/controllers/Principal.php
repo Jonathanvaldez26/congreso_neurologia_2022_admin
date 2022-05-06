6089829d3aa9b63731f3fdbc178494feb6a9ce6d
Gerson Velasco <gvelassco@gmail.com>
2022-05-06 16:54:59 -0500

    prueba

--- backend/App/controllers/Principal.php
+++ backend/App/controllers/Principal.php
@@ -62,7 +62,7 @@ html;
     //   View::set('utileriasHidden',$utileriasHidden);
 
       View::set('asideMenu',$this->_contenedor->asideMenu());
-    //   View::set('header',$this->_contenedor->header($extraHeader));
+      View::set('header',$this->_contenedor->header($extraHeader));
     //   View::set('footer',$this->_contenedor->footer($extraFooter));
       View::render("principal_all");
     }
